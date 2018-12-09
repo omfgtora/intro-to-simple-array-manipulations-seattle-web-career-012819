74283f2d7399d3a55b3e3243edacee1ae9d785ac
Ethan Roberts <ejrobe@gmail.com>
2018-12-09 21:51:59 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,27 @@
+def using_push
+  
+end
+
+def using_unshift
+  
+end
+
+def using_pop
+  
+end
+
+def pop_with_args
+  
+end
+
+def using_shift
+  
+end
+
+def shift_with_args
+  
+end
+
+def 
+  
+end
